fdc3a1ddaa472101d93c1314c07689b6020caf0a
Mateus <seanlilmateus@yahoo.de>
2012-06-29 12:27:11 +0300

    Update master

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,7 +1,7 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launch_opts)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds).tap do |win|
-		win.rootViewController = BBViewController.alloc.init 
+	win.rootViewController = BBViewController.alloc.init 
     	win.makeKeyAndVisible
     end
     true
